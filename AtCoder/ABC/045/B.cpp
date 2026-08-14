c06f71b9caca4f251e66b54454f8485cebd8d0de
Jay <20131a05a4@gmail.com>
2026-08-15 03:33:09 +0530

    Atcoder ABC045 : Pushed Failed Submission For Problem C

--- AtCoder/ABC/045/B.cpp
+++ AtCoder/ABC/045/B.cpp
@@ -5,12 +5,12 @@ int main(){
     string deck[3];
     cin >> deck[0] >> deck[1] >> deck[2];
 
+    for(int d : {0 , 1 , 3}) reverse(deck[d].begin() , deck[d].end());
+
     int turn = 0;
-    
     while(deck[turn].empty() == false){
         int next_turn = deck[turn].back() - 'a';
         deck[turn].pop_back();
-        
         turn = next_turn;
     }
 
